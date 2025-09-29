3ed3f3b88dc053b8d845af590f59bcb27190b747
VIktor Zhelizko <viktorchei1992@gmail.com>
2025-09-29 22:01:09 +0300

    run format

--- src/configuration/index.js
+++ src/configuration/index.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT
-const host = process.env.HOST
-const mongoURL = process.env.MONGO_URL
-module.exports = {port, host, mongoURL}
+const port = process.env.PORT;
+const host = process.env.HOST;
+const mongoURL = process.env.MONGO_URL;
+module.exports = { port, host, mongoURL };
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,26 @@
-const mongoose = require("mongoose")
-const validator = require("validator")
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is not correct');
+      }
     },
-    lastName: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error("Email is not correct")
-            }
-        },
-        required: true
-    }
-})
+    required: true,
+  },
+});
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model('User', userSchema);
 
-module.exports = User
+module.exports = User;
